3a1b2bf6eb3343e2b67fb256c205cdbe086e917f
psybuglite <itztonye@gmail.com>
2019-04-02 00:20:11 +0100

    modified the 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -12,6 +12,8 @@ const NotFoundPage = () => (
         <div className="clearfix maxwidth-sl wrapper-x desired-height-3qtr d-flx flex-dir-col j-c-c tx-c writing-m-top mx-auto">
           <h1>Whoa!!! That was weird.</h1>
           <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+          <br />
+          <p>Use the navigation above to get back to a more useful page.</p>
         </div>
     </section>
     <Footer />
