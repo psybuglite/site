97e4677c60df7bf8a11878126898dc03629dbd2a
Tonye Dickson <itztonye@gmail.com>
2020-08-16 09:06:16 +0100

    updated dependencies

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,22 +1,19 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import Footer from "../components/footer"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-
-    <section className="width-100-pc hide-flow-x main-top">
-        <div className="clearfix maxwidth-sl wrapper-x desired-height-3qtr d-flx flex-dir-col j-c-c tx-c writing-m-top mx-auto">
-          <h1>Whoa!!! That was weird.</h1>
-          <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-          <br />
-          <p>Use the navigation above to get back to a more useful page.</p>
-        </div>
+    <section className="width-100-pc page-padding">
+      <div className="clearfix maxwidth-sl wrapper-x desired-height-3qtr d-flx flex-dir-col j-c-c tx-c writing-m-top mx-auto">
+        <h1>Whoa!!! That was weird.</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <br />
+        <p>Use the navigation above to get back to a more useful page.</p>
+      </div>
     </section>
-    <Footer />
   </Layout>
 )
 
