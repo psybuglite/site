51f9c9fba9fc9d585f0ff6b9e20c98a8b666aca8
Tonye Dickson <itztonye@gmail.com>
2020-08-21 20:27:52 +0100

    tweaking blog content

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,6 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
-          `gatsby-remark-relative-images`,
           {
             resolve: `gatsby-remark-images`,
             options: {
